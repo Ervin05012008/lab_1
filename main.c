ea3d33c74d0cf280281756e90e33ff20e6add194
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-03-02 21:13:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 #include "LinkedListBidirectional.h"
 
 int main() {
-    LinkedList* list = createList();
-    loadList(list, "baza.txt");
+    LinkedList* list = create_list();
+    load_list(list, "baza.txt");
 
     int choice = -1, index;
     char nazwa[100], flaga;
     double gestosc;
-    struct Wezel* returnedWezel = NULL;
+    struct Node* returned_node = NULL;
 
     while (choice != 0) {
-        printActions();
+        print_actions();
         if (scanf("%d", &choice) != 1) { while(getchar() != '\n'); continue; }
 
         switch (choice) {
@@ -20,13 +20,13 @@ int main() {
             case 2:
                 printf("Name, Density, Flag: ");
                 scanf("%99s %lf %c", nazwa, &gestosc, &flaga);
-                push(list, createMaterial(nazwa, gestosc, flaga));
+                push(list, create_material(nazwa, gestosc, flaga));
                 break;
             case 3:
-                returnedWezel = pop(list);
-                if (returnedWezel) {
-                    printf("Removed: %s\n", returnedWezel->material->nazwa);
-                    freeWezel(returnedWezel);
+                returned_node = pop(list);
+                if (returned_node) {
+                    printf("Removed: %s\n", returned_node->material->nazwa);
+                    free_node(returned_node);
                 } else {
                     printf("List is empty.\n");
                 }
@@ -34,22 +34,22 @@ int main() {
             case 4:
                 printf("Index, Name, Density, Flag: ");
                 scanf("%d %99s %lf %c", &index, nazwa, &gestosc, &flaga);
-                pushAt(list, createMaterial(nazwa, gestosc, flaga), index);
+                push_at(list, create_material(nazwa, gestosc, flaga), index);
                 break;
             case 5:
                 printf("Index: "); scanf("%d", &index);
-                returnedWezel = popAt(list, index);
-                if (returnedWezel) {
-                    printf("Removed: %s\n", returnedWezel->material->nazwa);
-                    freeWezel(returnedWezel);
+                returned_node = pop_at(list, index);
+                if (returned_node) {
+                    printf("Removed: %s\n", returned_node->material->nazwa);
+                    free_node(returned_node);
                 } else {
                     printf("Failed to remove (incorrect index).\n");
                 }
                 break;
             case 6:
                 printf("Index: "); scanf("%d", &index);
-                returnedWezel = getElement(list, index);
-                if (returnedWezel) printf("Element: %s\n", returnedWezel->material->nazwa);
+                returned_node = get_element(list, index);
+                if (returned_node) printf("Element: %s\n", returned_node->material->nazwa);
                 else printf("Element not found.\n");
                 break;
             case 7: save(list, "baza.txt"); break;
@@ -57,6 +57,6 @@ int main() {
             default: printf("Input error.\n");
         }
     }
-    freeList(list);
+    free_list(list);
     return 0;
 }
