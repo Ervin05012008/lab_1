f27278e5781a18dab43414a75b90bc5508654424
Ervin Senkevich <146520901+Ervin518@users.noreply.github.com>
2026-03-05 08:36:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,23 +3,24 @@
 #include "LinkedListBidirectional.h"
 
 int main() {
-    LinkedList* list = create_list();
+    struct LinkedList* list = create_list();
     load_list(list, "baza.txt");
 
     int choice = -1;
     int index;
     char nazwa[100];
     double gestosc;
-    char flaga;
+    char flaga; 
     struct Node* temp = NULL;
 
     while (choice != 0) {
         print_actions();
         scanf("%d", &choice);
-        while (getchar() != '\n');
+        while (getchar() != '\n'); 
+
         switch (choice) {
-            case 1:
-                print(list);
+            case 1: 
+                print(list); 
                 break;
             case 2:
                 printf("Podaj: Nazwa Gestosc Flaga (np. Zelazo 7850 Z): ");
@@ -39,7 +40,7 @@ int main() {
                 push_at(list, create_material(nazwa, gestosc, flaga), index);
                 break;
             case 5:
-                printf("Podaj index do usuniecia: ");
+                printf("Podaj index do usuniecia: "); 
                 scanf("%d", &index);
                 temp = pop_at(list, index);
                 if (temp) {
@@ -48,23 +49,23 @@ int main() {
                 } else printf("Brak elementu.\n");
                 break;
             case 6:
-                printf("Podaj index do wyszukania: ");
+                printf("Podaj index do wyszukania: "); 
                 scanf("%d", &index);
                 temp = get_element(list, index);
                 if (temp) printf("Znaleziono: %s (%.2lf) [%c]\n", temp->material->nazwa, temp->material->gestosc, temp->material->flaga);
                 else printf("Brak elementu.\n");
                 break;
-            case 7:
-                save(list, "baza.txt");
+            case 7: 
+                save(list, "baza.txt"); 
                 printf("Zapisano!\n");
                 break;
-            case 0:
+            case 0: 
                 break;
-            default:
+            default: 
                 printf("Zly wybor!\n");
         }
     }
-
+    
     free_list(list);
     return 0;
 }
